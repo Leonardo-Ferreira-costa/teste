dd14b678a4030b1a1fc723dead4273da1d1d1267
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2022-09-27 15:12:05 -0300

    Add files via upload

---
+++ assets/src/cfg.php
@@ -0,0 +1,17 @@
+<?php
+/* Arquivo com as configurações do banco */
+// Variáveis do banco
+$servidor='localhost';
+$banco='senacmultimarcasbd';
+$usuarioBd='root';
+$senhaBd='';
+
+
+// Variavel que ira efetuar a conecxao com o banco de dados atraves dos valores recebidos das variaveis, caso haja falha ira exibir a menssagem Sem conexao
+$con = mysqli_connect($servidor,$usuarioBd,$senhaBd,$banco) or die
+ ("Sem conexão com o servidor!");
+ 
+?>
+
+
+
